e61a3bd638627c3e96c123307cf1fa7582575411
whiskie14142 <shushi.uetsuki@nifty.com>
2018-10-15 16:45:33 +0900

    Version 0.1.0

---
+++ test/test_read.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""Read SPK file by spktype21.
+The SPK file should be 'One object'.
+
+@author: Shushi Uetsuki (whiskie14142)
+"""
+
+from spktype21 import SPKType21
+import numpy as np
+
+print()
+path21 = input('Enter SPK file name (without extention) ? ') + '.bsp'
+k21 = SPKType21.open(path21)
+spkid = k21.segments[0].target
+
+print()
+print(k21)
+print()
+
+while(True):
+    jd = float(input('Enter JD or 999 for break ? '))
+    if jd <1000.0 : break
+    pos21, vel21 = k21.compute_type21(0, spkid, jd)
+    print('POS: ', pos21)
+    print('VEL: ', vel21)
+    print()
+
+k21.close()
